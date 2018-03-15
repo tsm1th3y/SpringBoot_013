01963ffb26c71fbf9853d01bf9e0d5ee0e3bee0f
tsmithe1 <tsmithe1@montgomerycollege.educd Desktop>
2018-03-15 01:54:45 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String loadData(){
+
+        Actor a = new Actor();
+        a.setName("Sandra Bullock");
+        a.setRealname("Sandra Mae Bullowski");
+        actorRepository.save(a);
+
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("About emojis ...");
+
+        actor.addMovie(movie);
+        actorRepository.save(actor);
+
+        model.addAttribute("actors", actorRepository.findAll());
+        return "index";
+    }
+}
